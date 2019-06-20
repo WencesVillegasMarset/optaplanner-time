9ce93a55738fe1e9058593842449e48d21e1036c
Danilo Reitano <daniloreitanoandrades@gmail.com>
2019-06-20 15:37:12 -0300

    Metodo para crear Timegrains

--- examples/sources/src/main/java/org/optaplanner/examples/audienciaTimeGrain/app/AudienciaCreator.java
+++ examples/sources/src/main/java/org/optaplanner/examples/audienciaTimeGrain/app/AudienciaCreator.java
@@ -4,22 +4,46 @@ import org.optaplanner.examples.audienciaTimeGrain.domain.AudienciaSchedule;
 import org.optaplanner.examples.audienciaTimeGrain.domain.Day;
 import org.optaplanner.examples.audienciaTimeGrain.domain.TimeGrain;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class AudienciaCreator {
 
-    void createTimeGrainList(AudienciaSchedule audienciaSchedule, int timeGrainListSize, int[] startingMinuteOfDayOptions) {
-        List<Day> dayList = new ArrayList<>(timeGrainListSize);
+    public void createTimeGrainList(LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime, AudienciaSchedule audienciaSchedule) {
+
+        // Crea la lista de startingMinuteOfDayOptions
+
+        long minutes = ChronoUnit.MINUTES.between(startTime, endTime);
+        long days = ChronoUnit.DAYS.between(startDate, endDate) + 1;
+        long timeGrainsPerDay = minutes / TimeGrain.GRAIN_LENGTH_IN_MINUTES;
+        long timeGrainListSize = timeGrainsPerDay * days;
+//        System.out.println(timeGrainListSize);
+        int[] startingMinuteOfDayOptions = new int[(int)timeGrainsPerDay];
+        int j = startTime.getHour()*60;
+        for(int i=0; i< timeGrainsPerDay; i++){
+            startingMinuteOfDayOptions[i] = j;
+            j+= TimeGrain.GRAIN_LENGTH_IN_MINUTES;
+        }
+
+
+
+        //Crea los TimeGrain y Day
+
+        List<Day> dayList = new ArrayList<>((int)timeGrainListSize);
         long dayId = 0;
         Day day = null;
-        List<TimeGrain> timeGrainList = new ArrayList<>(timeGrainListSize);
+        List<TimeGrain> timeGrainList = new ArrayList<>((int)timeGrainListSize);
+        int startingDay = startDate.getDayOfYear();
         for (int i = 0; i < timeGrainListSize; i++) {
             TimeGrain timeGrain = new TimeGrain();
             timeGrain.setIdTimeGrain(i);
-            int grainIndex = i;
-            timeGrain.setGrainIndex(grainIndex);
-            int dayOfYear = (i / startingMinuteOfDayOptions.length) + 1;
+            timeGrain.setGrainIndex(i);
+            int dayOfYear = (i / startingMinuteOfDayOptions.length) + startingDay;
             if (day == null || day.getDayOfYear() != dayOfYear) {
                 day = new Day();
                 day.setIdDay(dayId);
@@ -35,4 +59,6 @@ public class AudienciaCreator {
         audienciaSchedule.setDayList(dayList);
         audienciaSchedule.setTimeGrainList(timeGrainList);
     }
+
+
 }
--- examples/sources/src/main/java/org/optaplanner/examples/audienciaTimeGrain/app/Main.java
+++ examples/sources/src/main/java/org/optaplanner/examples/audienciaTimeGrain/app/Main.java
@@ -6,6 +6,8 @@ import org.optaplanner.core.api.solver.Solver;
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
+import java.time.LocalTime;
+import java.time.LocalDate;
 
 import org.optaplanner.core.api.solver.SolverFactory;
 import org.optaplanner.examples.audienciaTimeGrain.domain.Day;
@@ -16,56 +18,20 @@ import org.optaplanner.examples.audienciaTimeGrain.app.AudienciaCreator;
 public class Main {
     public static void main(String[] args) {
 
-        int[] startingMinuteOfDayOptions = {
-                8 * 60, // 08:00
-                8 * 60 + 15, // 08:15
-                8 * 60 + 30, // 08:30
-                8 * 60 + 45, // 08:45
-                9 * 60, // 09:00
-                9 * 60 + 15, // 09:15
-                9 * 60 + 30, // 09:30
-                9 * 60 + 45, // 09:45
-                10 * 60, // 10:00
-                10 * 60 + 15, // 10:15
-                10 * 60 + 30, // 10:30
-                10 * 60 + 45, // 10:45
-                11 * 60, // 11:00
-                11 * 60 + 15, // 11:15
-                11 * 60 + 30, // 11:30
-                11 * 60 + 45, // 11:45
-                13 * 60, // 13:00
-                13 * 60 + 15, // 13:15
-                13 * 60 + 30, // 13:30
-                13 * 60 + 45, // 13:45
-                14 * 60, // 14:00
-                14 * 60 + 15, // 14:15
-                14 * 60 + 30, // 14:30
-                14 * 60 + 45, // 14:45
-                15 * 60, // 15:00
-                15 * 60 + 15, // 15:15
-                15 * 60 + 30, // 15:30
-                15 * 60 + 45, // 15:45
-                16 * 60, // 16:00
-                16 * 60 + 15, // 16:15
-                16 * 60 + 30, // 16:30
-                16 * 60 + 45, // 16:45
-                17 * 60, // 17:00
-                17 * 60 + 15, // 17:15
-                17 * 60 + 30, // 17:30
-                17 * 60 + 45, // 17:45
-        };
-
         AudienciaSchedule audienciaSchedulePrueba = new AudienciaSchedule();
-        int timeGrainListSize = 80;
         AudienciaCreator audienciaCreator = new AudienciaCreator();
-        audienciaCreator.createTimeGrainList(audienciaSchedulePrueba, timeGrainListSize, startingMinuteOfDayOptions);
+
+        LocalDate startdate = LocalDate.of(2019,3,4);
+        LocalDate enddate = LocalDate.of(2019,3,6);
+        LocalTime startTime = LocalTime.of(9,00);
+        LocalTime endTime = LocalTime.of(11, 00);
+
+        audienciaCreator.createTimeGrainList(startdate, enddate, startTime, endTime, audienciaSchedulePrueba);
 
         List<TimeGrain> timeGrainList = audienciaSchedulePrueba.getTimeGrainList();
         for (TimeGrain timeGrain: timeGrainList) {
             System.out.println(timeGrain);
         }
-
-
 
 
         // TODO CAMBIAR all ESTO

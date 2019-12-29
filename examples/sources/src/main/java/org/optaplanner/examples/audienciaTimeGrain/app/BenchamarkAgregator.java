6a1cd0925b28ec9b0750d12cc4ef4a0ccfa350f0
Danilo Reitano <daniloreitanoandrades@gmail.com>
2019-12-29 18:25:40 -0300

    Benchmarking wip

--- examples/sources/src/main/java/org/optaplanner/examples/audienciaTimeGrain/app/BenchamarkAgregator.java
+++ examples/sources/src/main/java/org/optaplanner/examples/audienciaTimeGrain/app/BenchamarkAgregator.java
@@ -1,15 +1,12 @@
 package org.optaplanner.examples.audienciaTimeGrain.app;
 
-import org.optaplanner.benchmark.api.PlannerBenchmark;
 import org.optaplanner.benchmark.api.PlannerBenchmarkFactory;
 import org.optaplanner.benchmark.impl.aggregator.swingui.BenchmarkAggregatorFrame;
 
-public class Benchmarker {
-    public static void main(String[] args) {
+public class BenchamarkAgregator {
+    public static void main(String[] args){
         PlannerBenchmarkFactory plannerBenchmarkFactory = PlannerBenchmarkFactory.createFromXmlResource(
                 "org/optaplanner/examples/audienciaTimeGrain/solver/audienciaBenchmarkConfig.xml");
-//        BenchmarkAggregatorFrame.createAndDisplay(plannerBenchmarkFactory);
-        PlannerBenchmark plannerBenchmark = plannerBenchmarkFactory.buildPlannerBenchmark();
-        plannerBenchmark.benchmarkAndShowReportInBrowser();
+        BenchmarkAggregatorFrame.createAndDisplay(plannerBenchmarkFactory);
     }
 }
